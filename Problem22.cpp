c22c042e8bcd46cff46f88ddb685c96f854e88b2
Kevin Thompson <kevthompson97@gmail.com>
2017-07-28 23:31:42 -0500

    Now reads the .txt file and stores it in arrays
    empty arrays are a problem

--- Problem22.cpp
+++ Problem22.cpp
@@ -6,39 +6,51 @@ int main() {
 
 	char c;
 	bool isaname = false;
+	int tempindex = 0;
 	int index = 0;
-	std::string names[6000];
+	char temp[11];
+	char names[5161][11];
 
 
 	std::fstream rawtext;
 	rawtext.open("names.txt");
 	if (!rawtext) {
 		std::cout << "Unable to open file" << std::endl;
+		
 		exit(1); // terminate with error
 	}
 
 	while (rawtext >> c) {
+		
 		if (c == '"') {
 			if (isaname) {
 				//TODO
 				//save temporary char array as a string in names[]
-				
+				for (int j = 0; j < tempindex; j++) {
+					names[index][j] = temp[j];
+				}
+				tempindex = 0;
 				index++;
 			}
-			isaname == !isaname;
+			isaname = !isaname;
 		}
 		else if (isaname) {
-			//TODO
-			//add the character to a temporary array
+			temp[tempindex] = c;
+
+			tempindex++;
 		}
 	}
 	rawtext.close();
+
+	printf("names = %s", names[1]);
 
 	//sort names
 
 	//compute points of each name
 
 	//return total points
+
+	system("pause");
 
 
 	return 0;
