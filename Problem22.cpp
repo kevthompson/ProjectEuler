c23414d55f28a8205f9c73a5bbf028e953d5e5df
Kevin Thompson <kevthompson97@gmail.com>
2017-07-29 00:08:22 -0500

    Mostly done, just have to get my data sorted

--- Problem22.cpp
+++ Problem22.cpp
@@ -2,6 +2,22 @@
 #include <fstream>
 #include <string>
 
+//Stolen and Modified Code
+void insertion_sort(char arr[][11]) {
+	int j, temp;
+
+	for (int i = 0; i < 5162; i++) {
+		j = i;
+
+		while (j > 0 && arr[j] < arr[j - 1]) {
+			temp = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = temp;
+			j--;
+		}
+	}
+}
+
 int main() {
 
 	char c;
@@ -9,7 +25,7 @@ int main() {
 	int tempindex = 0;
 	int index = 0;
 	char temp[11];
-	char names[5161][11];
+	char names[5162][11];
 
 
 	std::fstream rawtext;
@@ -21,14 +37,15 @@ int main() {
 	}
 
 	while (rawtext >> c) {
-		
+
 		if (c == '"') {
 			if (isaname) {
-				//TODO
 				//save temporary char array as a string in names[]
 				for (int j = 0; j < tempindex; j++) {
 					names[index][j] = temp[j];
+
 				}
+				std::cout << std::endl;
 				tempindex = 0;
 				index++;
 			}
@@ -42,14 +59,28 @@ int main() {
 	}
 	rawtext.close();
 
-	printf("names = %s", names[1]);
 
+	int points[5162];
+	//TODO
 	//sort names
+	insertion_sort(names);
 
 	//compute points of each name
+	for (int j = 0; j < 5162; j++) {
+		int sum = 0;
+		for (int k = 0; k < 11; k++) {
+			sum += names[j][k];
+		}
+		points[j] = sum * (j + 1);
+	}
+
 
 	//return total points
-
+	int totalpoints = 0;
+	for (int j = 0; j < 5162; j++) {
+		totalpoints += points[j];
+	}
+	printf("\nThe total is %d\n", totalpoints);
 	system("pause");
 
 
