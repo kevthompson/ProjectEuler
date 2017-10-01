8e15dac2c9e642e69460512cf70b1604775a48a6
Kevin Thompson <krthmps2@linux-a2.ews.illinois.edu>
2017-10-01 18:46:05 -0500

    got Problem26 working

--- Problem26.c
+++ Problem26.c
@@ -2,38 +2,47 @@
 
 int main()
 {
-	int answer;
+	
+	int answer = 999;
 	int max = 0;
 	int len = 6;
-	int num = 1;
-	int den = 999;
+	unsigned long long num = 10;
+	int den = 3;
 	int done = 0;
+	//int rem = 0;
+	
 	while(!done)
 	{
-		if(((den%5) != 0) && ((den%2) != 0))
+                printf(" %llu / %d   ", num, den);
+		if((den%5) != 0)
 		{
-			num = 1;
-			len = 0;
-			printf("%d/%d   ", num, den);
-			while((num % den) != 1)
+			num = 1000;
+			len = 3;
+			//printf(" %llu / %d   ", num, 
+			//printf("%d  ", rem);
+
+			while(num != 1)
 			{
 				num *= 10;
+				num = num % den;
 				len += 1;
-
-				//printf("%d\n", num);
+				//printf(" %llu %d \n", num, den);
 			}
 			if(max < len)
 			{
+				printf("%d %d \n", answer, max);
 				max = len;
 				answer = den;			
 			}		
-			if(len > den)
+			if(den == 999)
 			{
 				done = 1;			
 			}
 		}
-		den--;
+		//done = 1;
+		den+=2;
+
 	}
-	printf("%d", answer);
+	printf("%d %d/n", answer, max);
 	return 0;
 }
